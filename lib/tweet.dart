1e9d4c7d43aa8338c718cb516e43cd94ebf674c1
Joshua Krinsky <44280379+joshpk29@users.noreply.github.com>
2020-08-06 15:52:13 -0400

    Set up to begin tweet search and sentiment analysis

---
+++ lib/tweet.dart
@@ -0,0 +1,26 @@
+//each tweet will be treated as an object and then data can be assigned and used as needed
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class tweet {
+  String Tweet;
+  int value;
+  var badWords = new List();
+  var badWordsVal = new List();
+  var goodWords = new List();
+  var goodWordsVal = new List();
+  tweet(String t, int v, List bw, List bwv, List gw, List gwv){
+    Tweet = t;
+    value = v;
+    badWords = bw;
+    badWordsVal = bwv;
+    goodWords = gw;
+    goodWordsVal = gwv;
+  }
+  Widget tweetAnalysisPage(BuildContext context){
+    return Scaffold(
+      
+    );
+  }
+}
