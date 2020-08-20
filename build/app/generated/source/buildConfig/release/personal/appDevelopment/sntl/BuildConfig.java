52743dfadbc43e2f955437cf97317700327e70d1
Joshua Krinsky <44280379+joshpk29@users.noreply.github.com>
2020-08-20 18:54:57 -0400

    Requirements for application release

---
+++ build/app/generated/source/buildConfig/release/personal/appDevelopment/sntl/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package personal.appDevelopment.sntl;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "personal.appDevelopment.sntl";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
